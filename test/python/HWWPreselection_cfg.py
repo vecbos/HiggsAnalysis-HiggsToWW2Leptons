05979ac1638931147a1bfdde5dc13a311207a8c5
emanuele <>
2008-09-24 13:29:18 +0000

    migration of the old cfg files

---
+++ test/python/HWWPreselection_cfg.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HWWPreselection")
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+
+process.load("HiggsAnalysis.HiggsToWW2Leptons.HWWPreselectionSequence_cff")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+process.source = cms.Source("PoolSource",
+                            debugFlag = cms.untracked.bool(True),
+                            debugVebosity = cms.untracked.uint32(10),
+                            fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/e/emanuele/RECO/relval_Zee_CMSSW_2_1_7.root')
+                            )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('histos.root')
+                                   )
+
+process.out = cms.OutputModule("PoolOutputModule",
+                               verbose = cms.untracked.bool(False),
+                               fileName = cms.untracked.string('preselection.root'),
+                               outputCommands = cms.untracked.vstring('drop *',
+                                                                      'keep *_*_*_HWWPreselection',
+                                                                      'keep *_muons_*_*',
+                                                                      'keep *_pixelMatchGsfElectrons_*_*'
+                                                                      )
+                               )
+
+
+process.p = cms.Path ( process.higgsToWW2LeptonsPreselectionSequence )
+
+process.o = cms.EndPath ( process.out )
+
+
